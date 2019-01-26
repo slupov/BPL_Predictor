8b8d3749475be94af7f872a60e2cb159a811d193
Slupov <stoyan.lupov@gmail.com>
2019-01-26 13:13:57 +0200

    [Create a data analysis model]

---
+++ mysite/polls/Data/Analysis/analysis_model.py
@@ -0,0 +1,138 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+ANALYSIS_ITERATIONS = 10
+TRAIN = 0
+TEST = 1
+
+
+def pad_string(string, empty_row_len):
+    return string.ljust(empty_row_len) + "|\n"
+
+
+class AnalysisModel:
+    def __init__(self, all_data_df, feature_names, classificator, model_name):
+        self.feature_names = feature_names
+        self.classificator = classificator
+
+        self.name = model_name
+
+        self.X = all_data_df[feature_names]
+        self.Y = all_data_df[classificator]
+
+        self.x_train = {}
+        self.x_test = {}
+        self.y_train = {}
+        self.y_test = {}
+
+        self.logistic_reg_avg = [0, 0]
+        self.decision_tree_avg = [0, 0]
+        self.k_neighbours_avg = [0, 0]
+
+        self.empty_row_len = 0
+
+    def __str__(self):
+        log_reg_avg_str = \
+            "|Accuracy of Logistic regression classifier on {} set: {:.2f}"
+
+        dec_tree_avg_str = \
+            "|Accuracy of Decision Tree classifier on {} set: {:.2f}"
+
+        knn_avg_str = "|Accuracy of K-NN classifier on {} set: {:.2f}"
+
+        header_str        = "+------------------- START %s -------------------+\n" % \
+                            self.name
+        footer_str        = "+-------------------- END %s --------------------+\n" % \
+                            self.name
+        empty_row_str     = "|                                                        " \
+                            "              |\n"
+
+        classificator_str = "|Classificator: '%s" % self.classificator
+        features_str = "|Feature names: %s" % self.feature_names
+
+        self.empty_row_len = len(empty_row_str) - 2
+
+        return "%s%s%s%s%s%s%s%s%s%s%s%s%s" % (
+                     header_str,
+
+                     self.get_string(classificator_str),
+                     self.get_string(features_str),
+
+                     empty_row_str,
+
+                     self.get_string(log_reg_avg_str.format("training",
+                                     self.logistic_reg_avg[TRAIN])),
+                     self.get_string(log_reg_avg_str.format("test",
+                                     self.logistic_reg_avg[TEST])),
+
+                     empty_row_str,
+
+                     self.get_string(dec_tree_avg_str.format("training",
+                                     self.decision_tree_avg[TRAIN])),
+                     self.get_string(dec_tree_avg_str.format("test",
+                                     self.decision_tree_avg[TEST])),
+
+                     empty_row_str,
+
+                     self.get_string(knn_avg_str.format("training",
+                                     self.k_neighbours_avg[TRAIN])),
+                     self.get_string(knn_avg_str.format("test",
+                                     self.k_neighbours_avg[TEST])),
+
+                     footer_str)
+
+    def get_string(self, string):
+        return pad_string(string, self.empty_row_len)
+
+    def test(self):
+        self.logistic_reg_avg[TRAIN] = 0
+        self.logistic_reg_avg[TEST] = 0
+
+        self.decision_tree_avg[TRAIN] = 0
+        self.decision_tree_avg[TEST] = 0
+
+        self.k_neighbours_avg[TRAIN] = 0
+        self.k_neighbours_avg[TEST] = 0
+
+        for i in range(0, ANALYSIS_ITERATIONS):
+            self.x_train, self.x_test, self.y_train, self.y_test = \
+                train_test_split(self.X, self.Y, random_state=0)
+
+            # -----------> logistic regression
+            self.logistic_reg_avg = \
+                [x + y for x, y in zip(self.logistic_reg_avg, self.logistic_regression())]
+
+            # -----------> decision_tree
+            self.decision_tree_avg = \
+                [x + y for x, y in zip(self.decision_tree_avg, self.decision_tree())]
+
+            # -----------> k neighbours
+            self.k_neighbours_avg = \
+                [x + y for x, y in zip(self.k_neighbours_avg, self.k_neighbours())]
+
+        # apply average
+        self.logistic_reg_avg = [x / ANALYSIS_ITERATIONS for x in self.logistic_reg_avg]
+        self.k_neighbours_avg = [x / ANALYSIS_ITERATIONS for x in self.k_neighbours_avg]
+        self.decision_tree_avg = [x / ANALYSIS_ITERATIONS for x in self.decision_tree_avg]
+
+    def logistic_regression(self):
+        logreg = LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=10000)
+        logreg.fit(self.x_train, self.y_train)
+
+        return [logreg.score(self.x_train, self.y_train),
+                logreg.score(self.x_test, self.y_test)]
+
+    def decision_tree(self):
+        clf = DecisionTreeClassifier().fit(self.x_train, self.y_train)
+
+        return [clf.score(self.x_train, self.y_train),
+                clf.score(self.x_test, self.y_test)]
+
+    def k_neighbours(self):
+        knn = KNeighborsClassifier()
+        knn.fit(self.x_train, self.y_train)
+
+        return [knn.score(self.x_train, self.y_train),
+                knn.score(self.x_test, self.y_test)]
--- mysite/polls/Data/Analysis/data_analysis.py
+++ mysite/polls/Data/Analysis/data_analysis.py
@@ -1,138 +1,96 @@
 import pandas as pd
-from matplotlib import cm
-from matplotlib import pyplot as plt
 from pandas.plotting import scatter_matrix
 
-from .dependence_graphs import generate_dependecy_graphs
-from ...models import ExtractedFixtures
-
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from matplotlib import cm
+from matplotlib import pyplot as plt
 
 from sklearn import preprocessing
-from sklearn import utils
+
+from ...models import ExtractedFixtures
+from ...models import MatchRawData
+
+from .analysis_model import AnalysisModel
+
+from .dependence_graphs import generate_dependecy_graphs
 
 
 def analyze_data():
-    all_data_df = pd.DataFrame.from_records(ExtractedFixtures.objects.all(). \
-                                            filter(season__gte="17/18"). \
-                                            order_by('season', 'home_team').values(),
-                                            exclude=['id'])
+    analyze_extracted_data()
+    # analyze_raw_match_data()
+
+
+def analyze_extracted_data():
+    extracted_data_df = pd.DataFrame.from_records(
+        ExtractedFixtures.objects.all().filter(season__gte="17/18").
+            order_by('season', 'home_team').values(), exclude=['id'])
+
     le = preprocessing.LabelEncoder()
-    all_data_df['home_team'] = le.fit_transform(all_data_df['home_team'])
-    all_data_df['away_team'] = le.fit_transform(all_data_df['away_team'])
-    all_data_df['result']=le.fit_transform(all_data_df['result'])
-    generate_dependecy_graphs(all_data_df)
+    extracted_data_df['home_team'] = le.fit_transform(extracted_data_df['home_team'])
+    extracted_data_df['away_team'] = le.fit_transform(extracted_data_df['away_team'])
+    extracted_data_df['result'] = le.fit_transform(extracted_data_df['result'])
 
-    feature_names = ['home_team','away_team', 'goal_diff', 'score_diff']
-    X = all_data_df[feature_names]
-    Y = all_data_df['result']
+    generate_dependecy_graphs(extracted_data_df)
 
-    generate_scatter_matrix(X, Y)
-
-    # apply min max normalization scaling to data
-
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=0)
-
-    # scaler = MinMaxScaler()
-    #
-    # X_train = scaler.fit_transform(X_train)
-    # X_test = scaler.transform(X_test)
+    feature_names = [['home_team', 'away_team', 'goal_diff', 'score_diff'],
+                     ['home_team', 'away_team', 'goal_diff', 'score_diff'],
+                     ['home_team', 'away_team', 'goal_diff', 'score_diff'],
+                     ['home_team', 'away_team', 'goal_diff', 'score_diff'],
+                     ['home_team', 'away_team', 'goal_diff', 'score_diff']]
 
     # BUILDING MODELS
 
-    # -----------> logistic regression
-    logistic_regression(X_train, Y_train, X_test, Y_test)
+    for i in range(0, 5):
+        model_name = "EXTRACTED DATA MODEL [%s]" % i
+        model = AnalysisModel(extracted_data_df, feature_names[i], 'result', model_name)
+        model.test()
+        print(model)
 
-    # -----------> decision_tree
-    decision_tree(X_train, Y_train, X_test, Y_test)
+        X = extracted_data_df[feature_names[i]]
+        Y = extracted_data_df['result']
 
-    # -----------> k neighbours
-    k_neighbours(X_train, Y_train, X_test, Y_test)
+        generate_scatter_matrix(X, Y, "extracted_fixtures", i)
 
-    # TODO Test for the rest from
-    # https://towardsdatascience.com/solving-a-simple-classification-problem-with-python-fruits-lovers-edition-d20ab6b071d2?fbclid=IwAR0kByuLFgCf3-pFw3Ff45g7I_e_yma0uxBmhilj2PA5m2RxShClYP-rSXM
-    # poisson_distribution(train_df, test_df)
-
-    print("debug")
+    print("Extracted data analysis ended.")
 
 
-def logistic_regression(X_train, Y_train, X_test, Y_test):
-    logreg = LogisticRegression(solver='lbfgs', multi_class='auto')
-    logreg.fit(X_train, Y_train)
+def analyze_raw_match_data():
+    extracted_data_df = pd.DataFrame.from_records(
+        MatchRawData.objects.all().filter(season__gte="17/18").
+            order_by('season', 'home_team').values(), exclude=['id'])
 
-    print('Accuracy of Logistic regression classifier on training set: {:.2f}'
-          .format(logreg.score(X_train, Y_train)))
-    print('Accuracy of Logistic regression classifier on test set: {:.2f}'
-          .format(logreg.score(X_test, Y_test)))
+    le = preprocessing.LabelEncoder()
+    extracted_data_df['home_team'] = le.fit_transform(extracted_data_df['home_team'])
+    extracted_data_df['away_team'] = le.fit_transform(extracted_data_df['away_team'])
+    extracted_data_df['result'] = le.fit_transform(extracted_data_df['result'])
 
+    generate_dependecy_graphs(extracted_data_df)
 
-def decision_tree(X_train, Y_train, X_test, Y_test):
-    clf = DecisionTreeClassifier().fit(X_train, Y_train)
-    print('Accuracy of Decision Tree classifier on training set: {:.2f}'
-          .format(clf.score(X_train, Y_train)))
-    print('Accuracy of Decision Tree classifier on test set: {:.2f}'
-          .format(clf.score(X_test, Y_test)))
+    feature_names = [
+        ['home_team', 'away_team', 'home_shots', 'away_shots'],
+        ['home_team', 'away_team', 'home_fouls_commited', 'away_fouls_commited'],
+        ['home_team', 'away_team', 'home_offsides', 'away_offsides'],
+        ['home_team', 'away_team', 'home_yellow_cards', 'away_yellow_cards'],
+        ['home_team', 'away_team', 'half_time_result']]
 
+    # BUILDING MODELS
 
-def k_neighbours(X_train, Y_train, X_test, Y_test):
-    knn = KNeighborsClassifier()
-    knn.fit(X_train, Y_train)
-    print('Accuracy of K-NN classifier on training set: {:.2f}'
-          .format(knn.score(X_train, Y_train)))
-    print('Accuracy of K-NN classifier on test set: {:.2f}'
-          .format(knn.score(X_test, Y_test)))
+    for i in range(0, 5):
+        model = AnalysisModel(extracted_data_df, feature_names[i], 'result')
+        model.test()
+        print(model)
 
+        X = extracted_data_df[feature_names]
+        Y = extracted_data_df['result']
 
-def poisson_distribution(train_df, test_df):
-    # get avarage column values
-    train_mean = train_df.mean()
-    test_mean = test_df.mean()
+        generate_scatter_matrix(X, Y, "extracted_fixtures", i)
 
-    print(train_mean, "\n\n")
-    print(test_mean, "\n\n")
-
-    # construct Poisson for each mean 'result' value {0, 0.5, 1}
-    # # (i - (0.5 * (i + 1)) is the result to predict
-    # poisson_pred = np.column_stack(
-    #     [[poisson.pmf((i - (0.5 * (i + 1))), train_mean['result']) for i in range(1, 4)]])
-    #
-    # # plot histogram of actual results
-    # plt.hist(train_df[['result']].values, range(3), alpha=0.7, label=['result'],
-    #          color=["#FFA07A"])
-    #
-    # # add lines for the Poisson distributions
-    # pois1, = plt.plot([i - 0.5 for i in range(1, 4)], poisson_pred[:1],
-    #                   linestyle='-', marker='o', label="Result", color='green')
-    #
-    # leg = plt.legend(loc='upper right', fontsize=13, ncol=2)
-    # leg.set_title("Poisson           Actual        ",
-    #               prop={'size': '14', 'weight': 'bold'})
-    #
-    # plt.xticks([i for i in range(1, 4)])
-    # plt.ylim([0.00, 1.00])
-    # plt.xlim([0.00, 1.00])
-    #
-    # plt.xlim(0.0, 0.5)
-    # plt.xlabel("Proportion of matches", size=13)
-    # plt.ylabel("Match result", size=13)
-    #
-    # plt.title("Just a stupid test, idk wtf i am doin lmao", size=14, fontweight='bold')
-    #
-    # plt.tight_layout()
-    # plt.show()
-
-    print("debug")
+    print("Extracted raw match data analysis ended.")
 
 
-def generate_scatter_matrix(trainX, trainY):
+def generate_scatter_matrix(trainX, trainY, model_type, model_idx):
     cmap = cm.get_cmap('gnuplot')
     scatter = scatter_matrix(trainX, c=trainY, marker='o', s=40, hist_kwds={'bins': 15},
                              figsize=(9, 9), cmap=cmap)
 
     plt.suptitle('Scatter-matrix for each input variable')
-    plt.savefig('extracted_fixtures_scatter_matrix')
+    plt.savefig('%s_scatter_matrix_%s' % (model_type, model_idx))
